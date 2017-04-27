d38ef1a0a07416ba8e3d3e4a8044d61bf7ff0126
A-Nosenko <nosenko-87@mail.ru>
2017-04-27 18:41:37 +0300

    fix transactional

--- src/main/java/place/to/time/service/UserServiceImpl.java
+++ src/main/java/place/to/time/service/UserServiceImpl.java
@@ -28,6 +28,7 @@ public class UserServiceImpl implements UserService {
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Transactional
     @Override
     public void save(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
